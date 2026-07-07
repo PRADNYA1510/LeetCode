c3a1eb041c133f8ac6abfc6fc4444285e1dd91d7
PRADNYA1510 <130041729+PRADNYA1510@users.noreply.github.com>
2026-07-07 22:12:25 +0530

    Time: 3 ms (25.57%) | Memory: 44.6 MB (18.97%) - LeetSync

--- 3684-substring-matching-pattern/substring-matching-pattern.java
+++ 3684-substring-matching-pattern/substring-matching-pattern.java
@@ -1,19 +1,13 @@
 class Solution {
     public boolean hasMatch(String s, String p) {
-        String[] str = p.split("\\*");
+        String[] str = p.split("\\*",-1);
         
         if(str.length==2){
         int sstr1 = s.indexOf(str[0]);
         int sstr2 = s.indexOf(str[1], sstr1 + str[0].length());
-        if(sstr1!=-1 && sstr2!=-1){
-            
-                return true;
-            
-            
+        if(sstr1!=-1 && sstr2!=-1){    
+                return true;  
         }
-        }
-        else if(str.length==0){
-            return true;
         }
         else{
            if(s.indexOf(str[0])!=-1){
